d9d1596e28c2716964244103439189eca60f4623
chrisli8 <8chrisgli@gmail.com>
2019-04-11 16:07:21 -0700

    color and other app specific designs added

--- Section Scroll/ViewController.swift
+++ Section Scroll/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstButton: UIButton!
     @IBOutlet weak var secondButton: UIButton!
     @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var secondSectionBtnView: UIView!
+    
+    // constaints for sections
     @IBOutlet weak var firstToTop: NSLayoutConstraint!
     @IBOutlet weak var secondToTop: NSLayoutConstraint!
     @IBOutlet weak var thirdToTop: NSLayoutConstraint!
@@ -38,8 +41,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.secondToTop.constant = self.view.frame.height - (150 + 48)
+        self.thirdToTop.constant = self.view.frame.height - (100 + 32)
+        secondSectionBtnView.layer.borderColor = UIColor.black.cgColor
+        secondSectionBtnView.layer.borderWidth = 2
+        secondSectionBtnView.backgroundColor = UIColor.clear
+        setBtnColorsDark()
     }
     
+    // Color to use (from app)
 //    UIColor(red: 255.0/255.0, green: 205.0/255.0, blue: 200.0/255.0, alpha: 1.0),
 //    UIColor(red: 252.0/255.0, green: 205.0/255.0, blue: 174.0/255.0, alpha: 1.0),
 //    UIColor(red: 252.0/255.0, green: 237.0/255.0, blue: 186.0/255.0, alpha: 1.0),
@@ -52,19 +62,19 @@ class ViewController: UIViewController {
         super .viewWillAppear(animated)
         firstView.backgroundColor = UIColor(red: 255.0/255.0, green: 205.0/255.0, blue: 200.0/255.0, alpha: 1.0)
         
-        secondToTop.constant = 100
+        //secondToTop.constant = 100
         secondView.backgroundColor = UIColor(red: 252.0/255.0, green: 205.0/255.0, blue: 174.0/255.0, alpha: 1.0)
-        secondView.layer.shadowColor = UIColor.black.cgColor
-        secondView.layer.shadowOpacity = 1
-        secondView.layer.shadowOffset = CGSize.zero
-        secondView.layer.shadowRadius = 3
+//        secondView.layer.shadowColor = UIColor.black.cgColor
+//        secondView.layer.shadowOpacity = 1
+//        secondView.layer.shadowOffset = CGSize.zero
+//        secondView.layer.shadowRadius = 3
         
-        thirdToTop.constant = 200
+        //thirdToTop.constant = 200
         thirdView.backgroundColor = UIColor(red: 252.0/255.0, green: 237.0/255.0, blue: 186.0/255.0, alpha: 1.0)
-        thirdView.layer.shadowColor = UIColor.black.cgColor
-        thirdView.layer.shadowOpacity = 1
-        thirdView.layer.shadowOffset = CGSize.zero
-        thirdView.layer.shadowRadius = 3
+//        thirdView.layer.shadowColor = UIColor.black.cgColor
+//        thirdView.layer.shadowOpacity = 1
+//        thirdView.layer.shadowOffset = CGSize.zero
+//        thirdView.layer.shadowRadius = 3
         
         roundButtonCorners(button: firstButton)
         roundButtonCorners(button: secondButton)
@@ -74,8 +84,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func firstButtonPressed(_ sender: Any) {
-        self.secondToTop.constant = self.view.frame.height - 150
-        self.thirdToTop.constant = self.view.frame.height - 100
+        self.secondToTop.constant = self.view.frame.height - (150 + 48)
+        self.thirdToTop.constant = self.view.frame.height - (100 + 32)
         
         zeroFirstBtnConstraints()
         setSecondBtnConstrains(to: 8.0)
@@ -99,8 +109,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func secondButtonPressed(_ sender: Any) {
-        secondToTop.constant = 50
-        thirdToTop.constant = view.frame.height - 100
+        secondToTop.constant = (50 + 16)
+        thirdToTop.constant = view.frame.height - (100 + 32)
         
         setFirstBtnConstrains(to: 8.0)
         zeroSecondBtnConstrains()
@@ -119,8 +129,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func thirdButtonPressed(_ sender: Any) {
-        secondToTop.constant = 50
-        thirdToTop.constant = 100
+        secondToTop.constant = (50 + 16)
+        thirdToTop.constant = (100 + 32)
         
         setFirstBtnConstrains(to: 8.0)
         setSecondBtnConstrains(to: 8.0)
@@ -179,6 +189,36 @@ class ViewController: UIViewController {
     func zeroBtnConstaints(top: NSLayoutConstraint, lead: NSLayoutConstraint,
                            trail: NSLayoutConstraint) {
         setBtnConstrains(top: top, lead: lead, trail: trail, to: 0)
+    }
+    
+//    UIColor(red: 210.0/255.0, green: 64.0/255.0, blue: 48.0/255.0, alpha: 1.0),
+//    UIColor(red: 223.0/255.0, green: 111.0/255.0, blue: 37.0/255.0, alpha: 1.0),
+//    UIColor(red: 248.0/255.0, green: 192.0/255.0, blue: 0.0/255.0, alpha: 1.0), //248, 192, 0 old yellow
+//    UIColor(red: 0.0/255.0, green: 186.0/255.0, blue: 76.0/255.0, alpha: 1.0),
+//    UIColor(red: 0.0/255.0, green: 201.0/255.0, blue: 227.0/255.0, alpha: 1.0),
+//    UIColor(red: 22.0/255.0, green: 53.0/255.0, blue: 166.0/255.0, alpha: 1.0), //32, 71, 215 old blue
+//    UIColor(red: 142.0/255.0, green: 41.0/255.0, blue: 229.0/255.0, alpha: 1.0),
+//    UIColor(red: 220.0/255.0, green: 44.0/255.0, blue: 98.0/255.0, alpha: 1.0)
+    func setBtnColorsDark() {
+        firstButton.backgroundColor = UIColor(red: 210.0/255.0, green: 64.0/255.0, blue: 48.0/255.0, alpha: 1.0)
+        firstButton.tintColor = UIColor.white
+        
+        secondButton.backgroundColor = UIColor(red: 223.0/255.0, green: 111.0/255.0, blue: 37.0/255.0, alpha: 1.0)
+        secondButton.tintColor = UIColor.white
+        
+        thirdButton.backgroundColor = UIColor(red: 248.0/255.0, green: 192.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        thirdButton.tintColor = UIColor.white
+    }
+    
+    func setBtnColorsLight() {
+        firstButton.tintColor = UIColor(red: 210.0/255.0, green: 64.0/255.0, blue: 48.0/255.0, alpha: 1.0)
+        firstButton.backgroundColor = UIColor.white
+        
+        secondButton.tintColor = UIColor(red: 223.0/255.0, green: 111.0/255.0, blue: 37.0/255.0, alpha: 1.0)
+        secondButton.backgroundColor = UIColor.white
+        
+        thirdButton.tintColor = UIColor(red: 248.0/255.0, green: 192.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        thirdButton.backgroundColor = UIColor.white
     }
 }
 
