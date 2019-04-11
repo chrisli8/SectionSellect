63c81f2d42d8fd995da3dca7d3d5d7acc66f0210
chrisli8 <8chrisgli@gmail.com>
2019-04-11 14:11:09 -0700

    views layed out properly

--- Section Scroll/ViewController.swift
+++ Section Scroll/ViewController.swift
@@ -15,12 +15,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstButton: UIButton!
     @IBOutlet weak var secondButton: UIButton!
     @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var firstToTop: NSLayoutConstraint!
+    @IBOutlet weak var secondToTop: NSLayoutConstraint!
+    @IBOutlet weak var thirdToTop: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super .viewWillAppear(animated)
+        secondToTop.constant = 100
+        secondView.backgroundColor = UIColor.blue
+        secondView.layer.shadowColor = UIColor.black.cgColor
+        secondView.layer.shadowOpacity = 1
+        secondView.layer.shadowOffset = CGSize.zero
+        secondView.layer.shadowRadius = 10
+        
+        thirdToTop.constant = 200
+        thirdView.backgroundColor = UIColor.purple
+        thirdView.layer.shadowColor = UIColor.black.cgColor
+        thirdView.layer.shadowOpacity = 1
+        thirdView.layer.shadowOffset = CGSize.zero
+        thirdView.layer.shadowRadius = 10
+        
+    }
 
-
+    @IBAction func firstButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func secondButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func thirdButtonPressed(_ sender: Any) {
+        
+    }
 }
 
