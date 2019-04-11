8ec81500e97aada841445dcaadc76d4fe4ed0da9
chrisli8 <8chrisgli@gmail.com>
2019-04-11 13:45:55 -0700

    first commit

---
+++ Section Scroll/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Section Scroll
+//
+//  Created by Chris Li on 4/11/19.
+//  Copyright © 2019 Chris Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var thirdView: UIView!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
